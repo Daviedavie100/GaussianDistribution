5b4364622231f0b48507e1cfd56c4f39a6909884
Daviedavie100 <innocentdavie@yahoo.com>
2024-02-11 22:57:21 +0300

    update

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 from setuptools import setup
 
-setup(name="distributions", 
- version="0.1",
- description="Gaussian distributions", 
- packages=['distributions'],
+setup(name="prob_distribution_packages", 
+ version='1.04.0',
+ description='Binomial and Gaussian distributions', 
+ packages=['prob_distribution_packages'],
+ author='David Owino',
+ author_email='dave.owino@yahoo.com',
  zip_safe=False)
